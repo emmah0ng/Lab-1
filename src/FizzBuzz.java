6bbaae86678187fa7557f969f5e873a92b63040e
Emma Hong <emmahong17@gmail.com>
2026-05-19 21:24:04 -0400

    Main

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,12 +6,12 @@ class FizzBuzz {
     public static void main(String[] args) {
         int i = 1;
         while (i < 100) {
-
-            i = doFizzBuzz(i);
+            doFizzBuzz(i);
+            i++;
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
@@ -34,7 +34,5 @@ class FizzBuzz {
             System.out.println(i);
 
         }
-        i++;
-        return i;
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,4 @@
+public class Main {
+    static void main() {
+        FizzBuzz.doFizzBuzz(5);
+    }}
